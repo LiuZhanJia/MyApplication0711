57d7ae40e2f781af2307eec2a14d10b314ac8d14
Jia <40441144>
2017-07-11 09:20:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jia/myapplication0711/MainActivity0711.java
@@ -0,0 +1,13 @@
+package com.example.jia.myapplication0711;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity0711 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main0711);
+    }
+}
